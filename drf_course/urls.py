583abaeb2cb06d46f506aae7b5d0f0e785bf7692
Baiel1208 <baiel000005@gmail.com>
2023-09-27 15:12:08 +0600

    drf course

---
+++ drf_course/urls.py
@@ -0,0 +1,55 @@
+"""drf_course URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib import admin
+from core import views as core_views
+from ecommerce import views as ecommerce_views
+from rest_framework import routers
+from rest_framework import permissions
+from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
+router = routers.DefaultRouter()
+router.register(r'item', ecommerce_views.ItemViewSet, basename='item')
+router.register(r'order', ecommerce_views.OrderViewSet, basename='order')
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
+    # docs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+]
